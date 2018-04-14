a9136a2795806413182263bb2c52bf5268702c0f
utkarshjogani <utkarshjogani@gmail.com>
2018-04-14 12:13:44 +0000

    front end start

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,5 +2,18 @@ var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
 
-
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'HELLO'
+	},
+        {
+            'sender': 'BOT',
+            'text': 'Hi, what can i do for you?'
+	},
+        {
+            'sender': 'USER',
+            'text': 'HELLO'
+	}
+   ];
 });
