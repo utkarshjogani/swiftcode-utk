a63aecc00c1921ebe582a83da82e8e1c586329a6
utkarshjogani <utkarshjogani@gmail.com>
2018-04-15 06:02:33 +0000

    message actor created

--- app/actors/MessageActor.java
+++ app/actors/MessageActor.java
@@ -1,20 +1,54 @@
 package actors;
 
+
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import data.FeedResponse;
 import data.Message;
+import data.NewsAgentResponse;
 import services.FeedService;
 import services.NewsAgentService;
 
+
 import java.util.UUID;
 
+import static data.Message.Sender.BOT;
+import static data.Message.Sender.USER;
+
 public class MessageActor extends UntypedActor {
+    public FeedService feedService = new FeedService();
+    public NewsAgentService newsAgentService = new NewsAgentService();
+    public NewsAgentResponse newsAgentResponse=new NewsAgentResponse();
+    private final ActorRef out;
+
+    public static Props props(ActorRef out) {
+        return Props.create(MessageActor.class, out);
+    }
+
+    public MessageActor(ActorRef out) {
+        this.out = out;
+    }
 
     @Override
     public void onReceive(Object message) throws Throwable {
+        ObjectMapper mapper = new ObjectMapper();
+        if (message instanceof String) {
+            Message messageObject = new Message();
+            messageObject.text=(String)message;
+            messageObject.sender= USER;
+            out.tell(mapper.writeValueAsString(messageObject), self());
+            String query=newsAgentService.getNewsAgentResponse(messageObject.text,UUID.randomUUID()).query;
+            FeedResponse feedResponse=feedService.getFeedQuery(newsAgentResponse.query);
+
+            messageObject.text = (feedResponse.title == null) ? "No results found" : "Showing results for: " + query;
+            messageObject.feedResponse=feedResponse;
+            messageObject.sender=BOT;
+            out.tell(mapper.writeValueAsString(messageObject), self());
+
+        }
 
     }
+
 }
