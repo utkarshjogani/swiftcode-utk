d1f58b7f49609949b44b5edb7497cdf40bea98d5
utkarshjogani <utkarshjogani@gmail.com>
2018-04-15 12:17:52 +0000

    deploy

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -8,7 +8,7 @@ app.config(function ($mdThemingProvider) {
 app.controller('chatController', function ($scope, $sce) {
 
 
-    var exampleSocket = new WebSocket('ws://localhost:9000/chatSocket');
+    var exampleSocket = new WebSocket('wss://swiftcode-newsbot-utk.herokuapp.com/chat');
     exampleSocket.onmessage = function (event) {
 
         var jsonData = JSON.parse(event.data);
